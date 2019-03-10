11d22981b0b95ccbe01baba4f6ee6da8ed5cf124
Faizan <faizan34@gmail.com>
2019-03-09 20:50:05 -0500

    edited to show temperatures

--- code/server/app.py
+++ code/server/app.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from flask import Markup
 from flask import render_template
+import sqlite3
 app = Flask(__name__)
 
 @app.route('/')
 def chart():
-    labels = ["Jan", "Feb", "Mar", "Apr", "May", "June", "Jul", "Aug"]
     #import from sqlite db
-    values = [10,9,8,7,6,4,7,8]
-    return render_template('main_chart.html', values=values, labels=labels)
-
+    db = sqlite3.connect('../../log/temperatures.db')
+    cursor = db.cursor()
+    cursor.execute('''SELECT temperature FROM recorded''')
+    values = []
+    for row in cursor:
+        raw_temperature = row[0]
+        #print('{0}'.format(raw_temperature[0:4]))
+        values.append(float(raw_temperature[0:4]))
+    #labels = ["Jan", "Feb", "Mar", "Apr", "May", "June", "Jul", "Aug"]
+    return render_template('main_chart.html', values=values)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
