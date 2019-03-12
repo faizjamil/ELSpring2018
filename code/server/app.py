0f706e9a6e2d6ea8d94c9fdcec186168513f1635
Faizan Jamil <faizan34@gmail.com>
2019-03-11 21:35:05 -0400

    server now collects temperature data upon startup

--- code/server/app.py
+++ code/server/app.py
@@ -2,20 +2,27 @@ from flask import Flask
 from flask import Markup
 from flask import render_template
 import sqlite3
+import importlib
+#importlib.import_module('blinkTemp.py')
+blink_module = importlib.import_module('blinkTemp')
 app = Flask(__name__)
 
 @app.route('/')
 def chart():
     #import from sqlite db
-    db = sqlite3.connect('../../log/temperatures.db')
+    blink_module.collectData()
+    db = sqlite3.connect('../log/temperatures.db')
     cursor = db.cursor()
-    cursor.execute('''SELECT temperature FROM recorded''')
-    values = []
+    cursor.execute('''SELECT time, temperature FROM recorded''')
+    temperatures = []
+    times = []
     for row in cursor:
-        raw_temperature = row[0]
+        time_elapsed = row[0]
+        raw_temperature = row[1]
         #print('{0}'.format(raw_temperature[0:4]))
-        values.append(float(raw_temperature[0:4]))
-    #labels = ["Jan", "Feb", "Mar", "Apr", "May", "June", "Jul", "Aug"]
-    return render_template('main_chart.html', values=values)
+        temperatures.append(float(raw_temperature[0:4]))
+        times.append(time_elapsed)
+    db.close()
+    return render_template('main_chart.html', values=temperatures, labels=times)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
